f792d58f8688d4d2550c11e3ec4944171f4d07c6
Yegor Belov <yegor@pleo.io>
2021-01-21 14:37:13 +0300

    prettier; try calling tumbrl

--- api/index.js
+++ api/index.js
@@ -1,9 +1,31 @@
-const app = require("express")();
+const app = require('express')()
+const tumblr = require('tumblr.js')
+const tumbrlClient = tumblr.createClient({
+  consumer_key: process.env.TUMBLR_API_KEY,
+})
 
-app.get("/tumblr", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
-  res.end(`sending tumbr info`);
-});
+function getTumblrPhotoPosts(blogName) {
+  return new Promise((resolve, reject) => {
+    tumbrlClient.blogPosts(blogName, { type: 'photo' }, function (err, resp) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(resp.posts)
+      }
+    })
+  })
+}
 
-module.exports = app;
+app.get('/tumblr', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate')
+  getTumblrPhotoPosts('rekall')
+    .then((posts) => res.end(posts))
+    .catch((err) => res.err(err))
+})
+
+app.get('*', function (req, res) {
+  res.send('🚫', 404)
+})
+
+module.exports = app
