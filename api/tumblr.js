c1c47fd4f7797b99a259ef17e181dc478fa91669
Yegor Belov <yegor@pleo.io>
2021-01-21 17:34:03 +0300

    add 1 hour cache for tumblr response

--- api/tumblr.js
+++ api/tumblr.js
@@ -1,24 +1,54 @@
 require('dotenv').config()
-// const NodeCache = require('node-cache')
+const NodeCache = require('node-cache')
 const tumblr = require('tumblr.js')
 const _sample = require('lodash.sample')
 const got = require('got')
-var express = require('express')
-var router = express.Router()
+const router = require('express').Router()
 
 const tumbrlClient = tumblr.createClient({
   consumer_key: process.env.TUMBLR_API_KEY,
 })
 
-function getTumblrPhotoPosts(blogName) {
+const tumblrCache = new NodeCache()
+
+const ONE_MINUTE = 60
+const ONE_HOUR = ONE_MINUTE * 60
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getTumblrPhotos(blogName) {
   return new Promise((resolve, reject) => {
-    tumbrlClient.blogPosts(blogName, { type: 'photo' }, function (err, resp) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(resp.posts)
-      }
-    })
+    const cached = tumblrCache.get(blogName)
+    if (cached) {
+      resolve(cached)
+      return
+    }
+    const total = tumblrCache.get(`${blogName}-total`) || 0
+    const totalPages = Math.floor(total / 20)
+
+    // if no cache, the offset is 0; after the 1st request we know the total amount of posts,
+    // so next requests (after posts cache expires), have a random offset
+    const postsFetchOffset = getRandomInt(0, totalPages)
+    tumbrlClient.blogPosts(
+      blogName,
+      { type: 'photo', offset: postsFetchOffset },
+      function (err, { posts, total_posts }) {
+        if (err) {
+          reject(err)
+        } else {
+          const photos = posts.map(getPostImage)
+
+          tumblrCache.set(blogName, photos, ONE_HOUR)
+          tumblrCache.set(`${blogName}-total`, total_posts)
+
+          resolve(photos)
+        }
+      },
+    )
   })
 }
 
@@ -32,10 +62,9 @@ router.get('/random-photo/:blogId', (req, res) => {
   res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate')
   res.setHeader('Content-Type', 'application/json')
 
-  getTumblrPhotoPosts(blogId)
-    .then((posts) => {
-      const images = posts.map(getPostImage)
-      const randomImage = _sample(images)
+  getTumblrPhotos(blogId)
+    .then((photos) => {
+      const randomImage = _sample(photos)
       got.stream(randomImage).pipe(res)
     })
     .catch((err) => {
